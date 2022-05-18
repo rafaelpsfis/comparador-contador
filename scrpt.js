04fb1beb8eaa8fa0806a5bd707415cf06ccf9f9d
Rafael Silva <rafaelps.fis@gmail.com>
2022-05-17 21:45:05 -0400

    new function added

--- scrpt.js
+++ scrpt.js
@@ -76,3 +76,72 @@ function contar() {
 }
 
 //======================== FIM CONTADOR ===========================
+
+//===================== CHAVE NFE ===============================
+
+function chaveNfe() {
+    let div = document.getElementById('programa')
+    div.classList.toggle("hide")
+    div.innerHTML = `
+        <div class="initializing-program">
+            <h1>Chave NF-e</h1>
+            <div class="input"> 
+                <input type="text" maxlength="70" class="field-program" id="item1" required>
+                <label>ITEM</label>
+            </div>
+    
+            <input type="button" value="Processar" class="btn-initial" onclick="decompoeNfe()">
+            <input type="button" value="X" class="btn-initial x" onclick="fechar()">
+            <p id="resultado"> Resultado: </p>
+        </div>
+    `
+}
+
+function decompoeNfe() {
+    let resultado = document.querySelector("#resultado")
+    if (item1.value == 0) {
+        window.alert("ERRO: Insira a CHAVE do documento fiscal no campo 'ITEM' e clique em 'Processar'!!")
+    } else if (item1.value.length !== 44) {
+        window.alert("ERRO: O número de caracteres precisa ser igual a 44 !!")
+    } else {
+        let codUf = item1.value.substring(0, 2)
+        let anoMes = item1.value.substring(2, 6)
+        let cnpj = item1.value.substring(6, 20)
+        let mod = item1.value.substring(20, 22)
+        let serie = item1.value.substring(22, 25)
+        let numDoc = item1.value.substring(25, 34)
+        let codDoc = item1.value.substring(34, 43)
+        let digVerif = item1.value.substring(43, 44)
+
+        resultado.innerHTML = `Código da UF do Emitente: ${codUf}`
+        resultado.innerHTML += ` AAMM de Emissão: ${anoMes}`
+        resultado.innerHTML += ` CNPJ do Emitente: ${cnpj}`
+        resultado.innerHTML += ` Modelo do documento: ${mod}`
+        resultado.innerHTML += ` Série do Documento: ${serie}`
+        resultado.innerHTML += ` Númerodo Documento: ${numDoc}`
+        resultado.innerHTML += ` Código Documento: ${codDoc}`
+        resultado.innerHTML += ` Dígito Verificador (DV): ${digVerif}`
+
+    }
+}
+
+ // EXEMPLO NFE
+ // 51080701212344000127550010000000981364112281
+
+ /* links consulta :
+ https://www.espiaonfe.com.br/lab/?i=como-montar-a-chave-de-acesso-do-ct-e
+
+ https://flexdocs.net/guiaCTe/funcao.utilidades.criachaveCTe.html
+
+ https://www.oobj.com.br/bc/article/como-%C3%A9-formada-a-chave-de-acesso-de-uma-nf-e-nfc-e-de-um-ct-e-e-um-mdf-e-281.html
+
+ http://www.sistemaunico.com.br/arquivos/manuais/chave_nfe.pdf
+
+ https://blog.tecnospeed.com.br/chave-de-acesso/
+
+ https://blog.emitte.com.br/chave-de-acesso-nota-fiscal-eletronica/
+ 
+ https://blog.tecnospeed.com.br/manifesto-documento-fiscal/#:~:text=O%20que%20%C3%A9%20Manifesto%20Eletr%C3%B4nico%20de%20Documentos%20Fiscais%20(MDFe)%3F&text=Sua%20principal%20fun%C3%A7%C3%A3o%20%C3%A9%20compilar,agilizando%20o%20processo%20de%20fiscaliza%C3%A7%C3%A3o.
+
+ https://www.espiaonfe.com.br/lab/?i=como-montar-a-chave-de-acesso-do-ct-e#:~:text=A%20Chave%20de%20Acesso%20do%20Conhecimento%20de%20Transporte%20Eletr%C3%B4nico%20(CT,C%C3%B3digo%20Num%C3%A9rico%20e%20D%C3%ADgito%20Verificador.
+ */
